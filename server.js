5ca7b7f974e818166b289f1fbba8952472ce072d
Shruti2906 <patilshruti2906@gmail.com>
2024-01-22 20:16:31 +0530

    added cors

--- server.js
+++ server.js
@@ -2,13 +2,28 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const router = require('./routes')
+const cors = require('cors')
+
 require('dotenv').config();
 
 const app = express();
 
-const PORT = process.env.PORT || 3000
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+        return res.status(200).json({});
+    }
+    next();
+});
 
 app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 4000
 
 mongoose.connect(process.env.DB_URL)
     .then(() => { console.log('Connected to MongoDB Atlas'); })
